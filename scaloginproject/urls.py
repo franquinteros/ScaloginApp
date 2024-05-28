ba8f3d5a6294394caca1cd801b2f4f66dc519803
franquinteros <frandecholila@gmail.com>
2024-05-28 16:42:54 -0300

    Forgot your password añadido. Version 1.3.5

--- scaloginproject/urls.py
+++ scaloginproject/urls.py
@@ -35,8 +35,8 @@ urlpatterns = [
     #Reset password
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset_form.html'), name='password_reset'),
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
-    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
     
 ] + static(settings.STATIC_URL,document_root= settings.STATIC_ROOT)
 
